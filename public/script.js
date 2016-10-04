4bd6d70bc37a8c10cb236d7bd93bb34d06337349
Lara Rubbelke <lara@rubbelke.com>
2016-10-04 00:56:57 -0700

    Fixes to site

--- public/script.js
+++ public/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
   $(window).on('resize', resize);
 
   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } }).then(function(stream) {
         video.src = window.URL.createObjectURL(stream);
         video.play();
     });
